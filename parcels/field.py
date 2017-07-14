746652ed0f9b289f611427e9d9c6b2041aa7cf8d
Erik van Sebille <erikvansebille@gmail.com>
2017-07-14 10:49:10 +0200

    Adding GeographicSquare and GeographicPolarSquare DistanceConverters
    Needed for converting distance in diffusion Kernels

--- parcels/field.py
+++ parcels/field.py
@@ -11,7 +11,8 @@ from datetime import timedelta, datetime
 from dateutil.parser import parse
 
 
-__all__ = ['CentralDifferences', 'Field', 'Geographic', 'GeographicPolar']
+__all__ = ['CentralDifferences', 'Field', 'Geographic', 'GeographicPolar',
+           'GeographicSquare', 'GeographicPolarSquare']
 
 
 class FieldSamplingError(RuntimeError):
@@ -122,6 +123,32 @@ class GeographicPolar(DistanceConverter):
 
     def ccode_to_target(self, x, y, z):
         return "(1.0 / (1000. * 1.852 * 60. * cos(%s * M_PI / 180)))" % y
+
+
+class GeographicSquare(DistanceConverter):
+    """ Square distance converter from geometric to geographic coordinates (m2 to degree2) """
+    source_unit = 'm2'
+    target_unit = 'degree2'
+
+    def to_target(self, value, x, y, z):
+        return (value / 1000. / 1.852 / 60.)**2
+
+    def ccode_to_target(self, x, y, z):
+        return "pow(1.0 / (1000.0 * 1.852 * 60.0), 2)"
+
+
+class GeographicPolarSquare(DistanceConverter):
+    """ Square distance converter from geometric to geographic coordinates (m2 to degree2)
+        with a correction to account for narrower grid cells closer to the poles.
+    """
+    source_unit = 'm2'
+    target_unit = 'degree2'
+
+    def to_target(self, value, x, y, z):
+        return (value / 1000. / 1.852 / 60. / cos(y * pi / 180))**2
+
+    def ccode_to_target(self, x, y, z):
+        return "pow(1.0 / (1000. * 1.852 * 60. * cos(%s * M_PI / 180)), 2)" % y
 
 
 class Field(object):
