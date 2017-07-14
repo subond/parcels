6f4e04cb43c9de30fd4ea84013a3ef6edfa3ae18
Erik van Sebille <erikvansebille@gmail.com>
2017-07-14 10:49:36 +0200

    Updating diffusion.py to use Kh_zonal and Kh_meridional fields

--- parcels/kernels/diffusion.py
+++ parcels/kernels/diffusion.py
@@ -1,4 +1,4 @@
-from parcels.rng import random
+from parcels import rng as random
 import math
 
 
@@ -7,11 +7,13 @@ __all__ = ['BrownianMotion2DFieldKh', 'SpatiallyVaryingDiffusion2D']
 
 def BrownianMotion2DFieldKh(particle, fieldset, time, dt):
     # Kernel for simple Brownian particle diffusion in zonal and meridional direction.
+    # Assumes that fieldset has fields Kh_zonal and Kh_meridional
 
-    kh = fieldset.Kh[time, particle.lon, particle.lat, particle.depth]
     r = 1/3.
-    particle.lat += random.uniform(-1., 1.)*math.sqrt(2*dt*kh/r)
-    particle.lon += random.uniform(-1., 1.)*math.sqrt(2*dt*kh/r)
+    kh_meridional = fieldset.Kh_meridional[time, particle.lon, particle.lat, particle.depth]
+    particle.lat += random.uniform(-1., 1.)*math.sqrt(2*dt*kh_meridional/r)
+    kh_zonal = fieldset.Kh_zonal[time, particle.lon, particle.lat, particle.depth]
+    particle.lon += random.uniform(-1., 1.)*math.sqrt(2*dt*kh_zonal/r)
 
 
 def SpatiallyVaryingDiffusion2D(particle, fieldset, time, dt):
